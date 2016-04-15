f4c57ff2233caed13764b71cd1c8fbc0eb3f74e1
AriGunawan <ari.gunawan@mitrais.com>
2016-04-15 15:40:03 +0800

    Add rubocop

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 
+# Class for test the Product model
 class ProductTest < ActiveSupport::TestCase
   def new_product(image_url)
     Product.new(title: 'My Book Title',
@@ -34,9 +35,9 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test 'image url' do
-    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
-             http://a.b.c/x/y/z/fred.gif }
-    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok = %w( fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif )
+    bad = %w( fred.doc fred.gif/more fred.gif.more )
     ok.each do |name|
       assert new_product(name).valid?, "#{name} should be valid"
     end
@@ -48,6 +49,15 @@ class ProductTest < ActiveSupport::TestCase
   test 'product is not valid without a unique title' do
     product = Product.new(title: products(:ruby).title,
                           description: 'description',
+                          price: 1,
+                          image_url: 'fred.gif')
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
+
+  test 'product is not valid without a unique title - i18n' do
+    product = Product.new(title: products(:ruby).title,
+                          description: 'yyy',
                           price: 1,
                           image_url: 'fred.gif')
     assert product.invalid?
